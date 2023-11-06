cd636a611bdc296cac716c7fa56153d4472cb758
Cris Huynh <119613966+CrisH2307@users.noreply.github.com>
2023-11-05 22:58:16 -0500

    push

--- server.js
+++ server.js
@@ -15,64 +15,6 @@
         URL: https://beautiful-blue-armadillo.cyclic.app
 *
 ********************************************************************************/
-const setData = require("../data/setData");
-const themeData = require("../data/themeData");
-
-let sets = [];
-
-const initialize = () => {
-  return new Promise((resolve, reject) => {
-    setData.forEach((setO) => {
-      const sameThing = themeData.find((themeO) => themeO.id === setO.theme_id);
-
-      if (sameThing) {
-        const newSetObj = {
-          ...setO,
-          theme: sameThing.name,
-        };
-
-        sets.push(newSetObj);
-      }
-    });
-    resolve();
-  });
-};
-
-const getAllSets = () => {
-  return new Promise((resolve, reject) => {
-    resolve(sets);
-  });
-};
-
-const getSetByNum = (setNum) => {
-  return new Promise((resolve, reject) => {
-    const found = sets.find((setObj) => setObj.set_num === setNum);
-
-    if (!found) {
-      reject("Unable to find requested");
-    } else {
-      resolve(found);
-    }
-  });
-};
-
-const getSetsByTheme = (theme) => {
-  return new Promise((resolve, reject) => {
-    const matchTheme = sets.filter((set) => set.theme.toLowerCase().includes(theme.toLowerCase()));
-
-    if (matchTheme.length === 0) {
-      reject("Unable to find");
-    } else {
-      resolve(matchTheme);
-    }
-  });
-};
-
-initialize();
-console.log(getAllSets());
-module.exports = { initialize, getAllSets, getSetByNum, getSetsByTheme };
-
-
 const legoData = require("./modules/legoSets");
 const path = require("path");
 
