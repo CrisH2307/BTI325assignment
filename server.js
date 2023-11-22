405f490b86eb67104dc73b0647dd647336769917
CrisH2307 <khangpicasso@gmail.com>
2023-11-22 14:47:51 -0500

    Remove large files

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /*********************************************************************************
-* BTI325 – Assignment 4
+* BTI325 – Assignment 5
 * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
 * No part of this assignment has been copied manually or electronically from any other source
 * (including web sites) or distributed to other students.
@@ -7,6 +7,7 @@
 * 
         Name: Cris Huynh 
         Student ID: 105444228 
+        Date: Nov 11th, 2023
         URL: https://beautiful-blue-armadillo.cyclic.app
 *
 ********************************************************************************/
@@ -15,13 +16,20 @@ const path = require("path");
 
 const express = require("express"); // "require" the Express module
 const app = express(); // obtain the "app" object
-const HTTP_PORT = process.env.PORT || 8080; // assign a port
+const HTTP_PORT = process.env.PORT || 8080; // assign a ports
 
 app.use(express.static("public")); // CSS
 app.use(express.static(path.join(__dirname, "public")));
 // Set EJS as the view engine
 app.set("view engine", "ejs");
 app.set("views", "./views"); // Specify the views directory
+
+//const legoSets = require("./path/to/legoSets"); // Import the legoSets module
+
+// Body parser middleware
+app.use(express.urlencoded({ extended: true }));
+
+express.urlencoded({ extended: true });
 
 // Route for the home page
 app.get("/", (request, response) => {
@@ -44,7 +52,7 @@ app.get("/lego/sets/", (request, response) => {
         });
     });
   } else {
-    //If no theme parameter, retrun all unfiltered Lego data
+    //If no theme parameter, return all unfiltered Lego data
     legoData.initialize().then(() => {
       legoData.getAllSets().then((sets) => {
         response.render("sets", { sets: sets });
@@ -74,22 +82,67 @@ app.get("/lego/sets/:set_num", (request, response) => {
     });
 });
 
-// Custom 404 route
-app.use((request, response) => {
-  const path = request.originalUrl;
-  let errorMessage = "The requested Lego set was not found";
-
-  if (path.includes("/lego/sets?theme=")) {
-    errorMessage = "No sets found for the specified theme.";
-    response.status(404).render("404", { message: errorMessage });
-  } else if (path.includes("/lego/sets/")) {
-    errorMessage = "No set found for the specified set number.";
-    response.status(404).render("404", { message: errorMessage });
-  } else {
-    errorMessage = "No view matched for the specific route.";
-    response.status(404).render("404", { message: errorMessage });
+// Render the addSet form
+app.get("/lego/addSet", async (req, res) => {
+  try {
+    const themes = await legoData.getAllThemes();
+    res.render("addSet", { themes });
+  } catch (err) {
+    res.status(500).render("500", { message: `Error: ${err.message}` });
   }
 });
+
+// Handle the submission of the addSet form
+app.post("/lego/addSet", async (req, res) => {
+  try {
+    const themes = await legoData.getAllThemes();
+    await legoData.addSet(req.body);
+    res.redirect("/lego/sets");
+  } catch (error) {
+    res
+      .status(500)
+      .render("500", { message: `I'm sorry, but we have encountered the following error: ${error.errors[0].message}` });
+  }
+});
+
+// GET route for editing a specific set
+app.get("/lego/editSet/:set_num", async (req, res) => {
+  try {
+    const set = await legoData.getSetByNum(req.params.set_num);
+    const themes = await legoData.getAllThemes();
+
+    res.render("editSet", { themes, set });
+  } catch (err) {
+    res.status(404).render("404", { message: err.message });
+  }
+});
+
+// POST route for updating a set
+app.post("/lego/editSet", async (req, res) => {
+  try {
+    await legoData.editSet(req.body.set_num, req.body);
+    res.redirect("/lego/sets");
+  } catch (err) {
+    res.status(500).render("500", {
+      message: `I'm sorry, but we have encountered the following error: ${err.errors[0].message || err}`,
+    });
+  }
+});
+
+// GET route for deleting a set
+app.get("/lego/deleteSet/:set_num", async (req, res) => {
+  try {
+    await legoData.deleteSet(req.params.set_num);
+    res.redirect("/lego/sets");
+  } catch (err) {
+    res.status(500).render("500", {
+      message: `I'm sorry, but we have encountered the following error: ${err.errors[0].message || err}`,
+    });
+  }
+});
+
+// Custom 404 route
+app.use((req, res) => res.status(404).render("404", { message: "Page not found" }));
 
 app.listen(HTTP_PORT, () => {
   console.log(`sever listening on ${HTTP_PORT}`);
