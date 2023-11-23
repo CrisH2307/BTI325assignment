9eb69f331e4cf73b43029c94d455f69f3d897d0c
CrisH2307 <khangpicasso@gmail.com>
2023-11-23 16:01:45 -0500

    OK

--- server.js
+++ server.js
@@ -8,12 +8,11 @@
         Name: Cris Huynh 
         Student ID: 105444228 
 <<<<<<< HEAD
-        Date: Nov 11th, 2023
-=======
 <<<<<<< HEAD
         Date: Nov 11th, 2023
 =======
 >>>>>>> 4d68244f7cbcd96c3c288d1466239162b347066c
+=======
 >>>>>>> origin/main
         URL: https://beautiful-blue-armadillo.cyclic.app
 *
@@ -23,7 +22,7 @@ const path = require("path");
 
 const express = require("express"); // "require" the Express module
 const app = express(); // obtain the "app" object
-const HTTP_PORT = process.env.PORT || 8080; // assign a ports
+const HTTP_PORT = process.env.PORT || 8080; // assign a port
 
 app.use(express.static("public")); // CSS
 app.use(express.static(path.join(__dirname, "public")));
@@ -89,11 +88,7 @@ app.get("/lego/sets/:set_num", (request, response) => {
     });
 });
 
-<<<<<<< HEAD
-// Render the addSet form
-=======
 // GET route for displaying the addSet view
->>>>>>> origin/main
 app.get("/lego/addSet", async (req, res) => {
   try {
     const themes = await legoData.getAllThemes();
@@ -103,27 +98,14 @@ app.get("/lego/addSet", async (req, res) => {
   }
 });
 
-<<<<<<< HEAD
-// Handle the submission of the addSet form
-=======
 // POST route for processing the addSet form
->>>>>>> origin/main
 app.post("/lego/addSet", async (req, res) => {
   try {
     const themes = await legoData.getAllThemes();
     await legoData.addSet(req.body);
     res.redirect("/lego/sets");
-<<<<<<< HEAD
-  } catch (error) {
-    res
-      .status(500)
-      .render("500", { message: `I'm sorry, but we have encountered the following error: ${error.errors[0].message}` });
-=======
   } catch (err) {
-    res
-      .status(500)
-      .render("500", { message: `I'm sorry, but we have encountered the following error: ${err.errors[0].message}` });
->>>>>>> origin/main
+    res.status(500).render("500", { message: `I'm sorry, but we have encountered the following error: ${err}` });
   }
 });
 
